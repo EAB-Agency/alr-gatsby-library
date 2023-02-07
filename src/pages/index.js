8577987dcadd5ef2f0ff8bfdb15878dd31be0fa8
Bob_Smith <bobsmith@eab.com>
2023-02-07 13:17:33 -0500

    update index to point to correct testimonial case

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Testimonial from '../components/social-proof/testimonial';
+import Testimonial from 'components/social-proof/Testimonial';
 
 
 const IndexPage = () => {
