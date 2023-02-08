edb3e92f50abe2985e1b28b14a313a192259f91d
Bob_Smith <bobsmith@eab.com>
2023-02-08 10:45:46 -0500

    updated a whole bunch of files
    In ex exercitation dolor irure deserunt do qui ut amet dolore do laboris anim Lorem laborum.

--- src/components/header/WithNav.jsx
+++ src/components/header/WithNav.jsx
@@ -4,7 +4,8 @@ import './header-nav-styles.scss';
 
 const WithNav = () => {
   return <div>
-    <nav><ul><li>list item</li></ul></nav>
+    <nav><ul><li>list item</li>
+    <li>list 2</li></ul></nav>
     <Header />
   </div>;
 };
