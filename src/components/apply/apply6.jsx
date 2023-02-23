9e6c2a099aad0cb573f94bff3dcb7651dece0f46
Lara <llatham@eab.com>
2023-02-23 14:17:21 -0500

    created style-apply-v6

---
+++ src/components/apply/apply6.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DefaultApply from './default-apply';
+import './style-apply-v6.scss';
+
+const applySix = () => (
+  <div className="apply-v6 layout-section">
+    <DefaultApply />
+  </div>
+);
+
+export default applySix;
