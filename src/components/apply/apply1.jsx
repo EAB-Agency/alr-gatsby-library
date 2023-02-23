ff76755e468bda51c13dcf01ce47d576dbe1bc67
Bob_Smith <bobsmith@eab.com>
2023-02-23 17:18:13 -0500

    add new SnippetToggler component

--- src/components/apply/apply1.jsx
+++ src/components/apply/apply1.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable import/no-webpack-loader-syntax */
 import React from 'react';
+import SnippetToggler from '../utils/SnippetToggler';
 import DefaultApply from './default-apply';
+
 import './style-apply-v1.scss';
+
+// eslint-disable-next-line import/no-unresolved
+const styleSheet = require('!!raw-loader!./style-apply-v1.scss');
 
 const applyOne = () => (
   <div className="apply-v1 layout-section">
-    {/* <button className="button" type="button">
-			hello button
-		</button> */}
     <DefaultApply />
+    <SnippetToggler fileName="style-apply-v1.scss" code={styleSheet.default} />
   </div>
 );
 
