74263635fbda13338b693ef408f25a4771689376
Bob_Smith <bobsmith@eab.com>
2023-02-24 13:25:43 -0500

    use css modules approach

--- src/components/apply/apply1.jsx
+++ src/components/apply/apply1.jsx
@@ -2,20 +2,15 @@
 import React from 'react';
 import SnippetToggler from '../utils/SnippetToggler';
 import DefaultApply from './default-apply';
-
-import './style-apply-v1.scss';
+import * as styles from './style-apply-v1.scss';
 
 // eslint-disable-next-line import/no-unresolved
 const styleSheet = require('!!raw-loader!./style-apply-v1.scss');
 
 const applyOne = () => (
-  <div className="apply-v1 layout-section">
+  <div className={styles.lpApply}>
     <DefaultApply />
-    <SnippetToggler
-      fileName="style-apply-v1.scss"
-      code={styleSheet.default}
-      lineNumbersToHide="1-3"
-    />
+    <SnippetToggler fileName="style-apply-v1.scss" code={styleSheet.default} />
   </div>
 );
 
--- src/components/give/give1.jsx
+++ src/components/give/give1.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import DefaultGive from './default-give';
-import './style-give-v1.scss';
+import SnippetToggler from '../utils/SnippetToggler';
+
+import * as styles from './style-give-v1.scss';
+
+// eslint-disable-next-line import/no-unresolved
+const styleSheet = require('!!raw-loader!./style-give-v1.scss');
 
 const GiveOne = () => (
-  <div className="give-v1 layout-section">
+  <div className={styles.lpIntroForm}>
     <DefaultGive />;
+    <SnippetToggler fileName="style-give-v1.scss" code={styleSheet.default} />
   </div>
 );
 
