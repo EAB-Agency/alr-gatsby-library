32e47052cae0ee0fbbf9e4018551affbe2087edb
Eric Klemen <emkmail2@gmail.com>
2023-02-27 17:21:21 -0500

    Update markup for footer 1-3, add footer 4

--- src/components/footer/footer4.jsx
+++ src/components/footer/footer4.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import DefaultFooter from './default-footer';
-import './style-footer-v2.scss';
+import './style-footer-v4.scss';
 
-const FooterTwo = () => (
-  <div className="footer-v2 layout-section">
+const FooterFour = () => (
+  <div className="footer-v4 layout-section">
     <footer className="school-footer">
       <div className="school-footer-wrapper center">
-        <div className="school-footer-name">the School Name</div>
-
         <div className="school-footer-content">
-          <address className="school-footer-address">Street Address<br />
-          TBD, 2-digit state code #####</address>
+          <div className="school-footer-name">the School Name</div>
+
+          <address className="school-footer-address">
+            Street Address, <span className="line-break">TBD, 2-digit state code #####</span>
+          </address>
 
           <ul className="school-footer-links">
             <li><a data-fancybox="help-modal" data-modal="true" data-src="#help-modal" href="#">Technical Help</a></li>
@@ -22,4 +23,4 @@ const FooterTwo = () => (
   </div>
 );
 
-export default FooterTwo;
+export default FooterFour;
