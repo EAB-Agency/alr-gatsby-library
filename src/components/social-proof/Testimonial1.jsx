68b1e3d9faba83aee406374b57bdcc50b1e3424f
natestamour <124597160+natestamour@users.noreply.github.com>
2023-02-27 15:46:12 -0500

    Cleaned up testimonial components

---
+++ src/components/social-proof/Testimonial1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DefaultTestimonial from './default-testimonial';
+import './style-testimonial-v1.scss';
+
+const TestimonialOne = () => (
+  <div className="testimonial-v1 layout-section">
+      <DefaultTestimonial />
+  </div>
+);
+export default TestimonialOne;
