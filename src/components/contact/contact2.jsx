d9bdaa54ca267c0d0c28357a8870992eedbbd47e
Eric Klemen <emkmail2@gmail.com>
2023-02-28 14:12:38 -0500

    Add contact layout options 1-3

--- src/components/contact/contact2.jsx
+++ src/components/contact/contact2.jsx
@@ -4,7 +4,29 @@ import './style-contact-v2.scss';
 
 const contactTwo = () => (
   <div className="contact-v2 layout-section">
-    <DefaultContact />;
+    <section className="lp-school-contact">
+      <div className="center lp-school-wrapper">
+        <div className="lp-school-contact-info">
+          <div className="lp-school-contact-intro">
+            <h3>Let’s Get Connected</h3>
+            <p>
+              Earning your master’s degree is an important step in your career,
+              and we are here to support you. Don’t hesitate to reach out to get
+              answers to any questions you may have!
+            </p>
+          </div>
+          
+          <ul className="lp-school-contact-options">
+            <li className="lp-school-contact-phone">
+              <a href="tel:xxx-xxx-xxxx">xxx-xxx-xxxx</a>
+            </li>
+            <li className="lp-school-contact-email">
+              <a href="mailto:help@schooldomain.edu">help@schooldomain.edu</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
