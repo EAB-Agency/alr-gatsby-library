76eb18acc3c05d4e653742626f4a2fdc08eb63eb
natestamour <124597160+natestamour@users.noreply.github.com>
2023-03-03 17:28:17 -0500

    Added Testimonial v1 styles

--- src/components/social-proof/default-testimonial.js
+++ src/components/social-proof/default-testimonial.js
@@ -4,7 +4,7 @@ const DefaultTestimonial = () => (
   <div className="social-proof">
     <div className="center">
       <div className="social-proof-quote">
-        <div className="img-wrap"><img alt="Meghan Munsey" className="social-proof-photo" height="100" width="100" src="https://admiss.info/assets/lp-live/712-mbu-a-live/img-meghan-munsey.png" /></div>
+        <div className="img-wrap"><img alt="Sneha Soni" className="social-proof-photo" height="100" width="100" src="https://admiss.info/assets/lp-live/1537-hoo-g-live/img-sneha.jpeg" /></div>
 
         <blockquote>
         <p>Whether through our flexible online programs or the option to turn real-life experience into college credit, it's our mission to assist our hardworking students however we can. The dedication our graduate students show in their careers, academics, and personal responsibilities inspires us every day. We're proud to be there for them during each step of their journey at MBU.</p>
