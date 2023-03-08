55d802a4a8d2d1b4f82f6a59ff85f11fb59af03b
natestamour <124597160+natestamour@users.noreply.github.com>
2023-03-08 10:59:40 -0500

    Added more code toggles.

--- src/components/footer/footer3.jsx
+++ src/components/footer/footer3.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import DefaultFooter from './default-footer';
+import SnippetToggler from '../utils/SnippetToggler';
 import './style-footer-v3.scss';
+
+// eslint-disable-next-line import/no-unresolved
+const styleSheet = require('!!raw-loader!./style-footer-v3.scss');
+// const html = require('!!raw-loader!./apply1.html');
+
+const codeFiles = [
+  {
+    fileName: 'style-footer-v3.scss',
+    code: styleSheet.default,
+    language: 'scss',
+    lineNumbersToDelete: '1-6',
+  },
+  // {
+  //   fileName: 'apply1.html',
+  //   code: html.default,
+  //   language: 'html',
+  // },
+];
 
 const FooterThree = () => (
   <div className="footer-v3 layout-section">
     <DefaultFooter />;
+    <SnippetToggler files={codeFiles} />
   </div>
 );
 
