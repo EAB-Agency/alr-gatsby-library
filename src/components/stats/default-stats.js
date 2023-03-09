cde5b22fe17733663bcaa488dd9306ef21a331cf
Lara <llatham@eab.com>
2023-03-09 11:50:27 -0500

    stats html

--- src/components/stats/default-stats.js
+++ src/components/stats/default-stats.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 const DefaultStats = () => (
   <div className="stats-section">
-
+    <div className="center">
+      <ul className="lp-stats-list">
+        <li className="lp-stats-item">
+          <div className="emphasis">#5</div>
+          <div className="description">Best Business School</div>
+          <div className="source">
+            <em>U.S. News & World Report</em>
+          </div>
+        </li>
+        <li className="lp-stats-item">
+          <div className="emphasis">Top 10</div>
+          <div className="description">Best School in the West</div>
+          <div className="source">
+            <em>U.S. News & World Report</em>
+          </div>
+        </li>
+        <li className="lp-stats-item">
+          <div className="emphasis">Best</div>
+          <div className="description">in the Nation for Business</div>
+          <div className="source">
+            <em>U.S. News & World Report</em>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
