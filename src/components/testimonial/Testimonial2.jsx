2ba3110395716bf11e875b2257fc3561e0954409
St. Amour, Nathaniel <nst.amour@eab.com>
2023-06-12 16:42:36 -0400

    Testimonial 2 Styles

--- src/components/testimonial/Testimonial2.jsx
+++ src/components/testimonial/Testimonial2.jsx
@@ -5,7 +5,7 @@ import './style-testimonial-v2.scss';
 
 // eslint-disable-next-line import/no-unresolved
 const styleSheet = require('!!raw-loader!./style-testimonial-v2.scss');
-// const html = require('!!raw-loader!./apply1.html');
+const html = require('!!raw-loader!./default-testimonial.html');
 
 const codeFiles = [
   {
@@ -14,17 +14,31 @@ const codeFiles = [
     language: 'scss',
     lineNumbersToDelete: '1-6',
   },
-  // {
-  //   fileName: 'apply1.html',
-  //   code: html.default,
-  //   language: 'html',
-  // },
+  {
+    fileName: 'default-testimonial.html',
+    code: html.default,
+    language: 'html',
+  },
 ];
 
-const TestimonialTwo = () => (
-  <div className="testimonial-v2 layout-section">
-      <DefaultTestimonial />
-      <SnippetToggler files={codeFiles} />
+const TestimonialTwo= () => (
+  <div className="🚀-testimonial-v2 layout-section">
+    <div className="lp-testimonial">
+      <div className="center testimonial-wrapper">
+        <div className="testimonial-container">          
+          <div className="testimonial-quote">
+            <blockquote>
+            <p>Whether through our flexible online programs or the option to turn real-life experience into college credit, it's our mission to assist our hardworking students however we can. The dedication our graduate students show in their careers, academics, and personal responsibilities inspires us every day. We're proud to be there for them during each step of their journey at MBU.</p>
+            </blockquote>
+            <div class="img-cite">
+              <img alt=" " className="testimonial-photo" height="100" width="100" src="https://admiss.info/assets/lp-live/1537-hoo-g-live/img-sneha.jpeg" />
+              <cite>Meghan Munsey<br/>Associate Director of First Year Experience<br/>MBU&nbsp;Online</cite>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <SnippetToggler files={codeFiles} />
   </div>
 );
 export default TestimonialTwo;
